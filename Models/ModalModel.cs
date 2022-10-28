be6bc846d7d9f399a2cbcd459af289cfa2ffe872
MMS <35738176+SK68-ph@users.noreply.github.com>
2022-10-28 18:05:40 +0800

    Initial Commit

---
+++ Models/ModalModel.cs
@@ -0,0 +1,24 @@
+namespace YetAnotherNoteSaver.Models
+{
+    public class ModalModel : NoteModel
+    {
+        public string Display { get; set; } = "none;";
+        public string Class { get; set; } = "";
+        public bool Backdrop { get; set; } = false;
+
+
+        public void Open()
+        {
+            this.Display = "block;";
+            this.Class = "show";
+            this.Backdrop = true;
+        }
+
+        public void Close()
+        {
+            this.Display = "none";
+            this.Class = "";
+            this.Backdrop = false;
+        }
+    }
+}
---
+++ Models/NoteModel.cs
@@ -0,0 +1,10 @@
+﻿namespace YetAnotherNoteSaver.Models
+{
+    public class NoteModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string NoteDescription { get; set; }
+        public string color { get; set; }
+    }
+}
